87f299739f1750e553595d774ec3757287df5046
Valentin Harrang <valhrg@gmail.com>
2025-12-13 13:07:42 +0100

    chore(vite.config): configure vite proxy for yahoo finance

--- vite.config.ts
+++ vite.config.ts
@@ -10,4 +10,13 @@ export default defineConfig({
       "@": path.resolve(__dirname, "src"),
     },
   },
+  server: {
+    proxy: {
+      '/api/yahoo': {
+        target: 'https://query1.finance.yahoo.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api\/yahoo/, ''),
+      },
+    },
+  },
 })
